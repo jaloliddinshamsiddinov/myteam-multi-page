18f5d640bb47c8b1a6619febd3c4db2016cb2a2a
jaloliddinshamsiddinov <125132177+jaloliddinshamsiddinov@users.noreply.github.com>
2023-07-17 01:57:22 +0500

    update/footer

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-let burger = document.getElementById("header-burger")
-let menu = document.getElementById("header_menu")
-let logo = document.getElementById("header_logo")
+let burger = document.getElementById("header_burger");
 
-burger.addEventListener('click', () => {
-    menu.classList.toggle("header_menu-active")
-    burger.classList.toggle("header_burger-active")
-    logo.classList.toggle("header_logo-active")
-})
+burger.addEventListener("click", () => {
+  document.getElementById("menu-bar").classList.toggle("menu-bar-active");
+});
+
+document.getElementById("header_burger__in").addEventListener("click", () => {
+  document.getElementById("menu-bar").classList.toggle("menu-bar-active");
+});
